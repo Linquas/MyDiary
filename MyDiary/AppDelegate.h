a3414c18845c71d044a6edc85497d0713ce95bac
Linquas <steven84059@gmail.com>
2017-08-30 13:16:46 +0800

    Initial Commit

---
+++ MyDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDiary
+//
+//  Created by Linquas on 30/08/2017.
+//  Copyright © 2017 Linquas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDiary
+//
+//  Created by Linquas on 30/08/2017.
+//  Copyright © 2017 Linquas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
